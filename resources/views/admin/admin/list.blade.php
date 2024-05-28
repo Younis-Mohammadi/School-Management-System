603f9d0fcddb95e6576b4c55a7edb6b9c69e8c5b
Younis-Mohammadi <artacodding2023@gmail.com>
2024-05-28 10:42:32 +0430

    admin search completed

--- resources/views/admin/admin/list.blade.php
+++ resources/views/admin/admin/list.blade.php
@@ -6,10 +6,42 @@
     <div class="d-flex justify-content-between align-items-center flex-wrap mb-3">
         @include('_message')
         <div>
-            <h1>Admin List</h1>
+            <h1>Admin List (Total :{{$getRecord->total()}})</h1>
         </div>
     </div>
-
+    <div class="row">
+        <div class="col-md-12 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <form class="forms-sample" method="get">
+                        @csrf
+                        <div class="row mb-3">
+                            <div class="col-md-3">
+                                <label class="form-label">Name</label>
+                                <input type="text" class="form-control mb-4 mb-md-0" value="{{Request::get('name')}}"
+                                    name="name" placeholder="Name">
+                            </div>
+                            <div class="col-md-3">
+                                <label class="form-label">Email</label>
+                                <input type="text" class="form-control" name="email" placeholder="Email"
+                                    value="{{Request::get('email')}}">
+                            </div>
+                            <div class="col-md-3">
+                                <label class="form-label">Date</label>
+                                <input type="date" class="form-control" name="date" value="{{Request::get('date')}}">
+                            </div>
+                            <div class="col-md-3">
+                                <button type="submit" class="btn btn-inverse-primary"
+                                    style="margin-top: 22px;">Search</button>
+                                <a href="{{url('admin/admin/list')}}" class="btn btn-inverse-danger"
+                                    style="margin-top: 22px;">Reset</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="row">
         <div class="col-md-12 grid-margin stretch-card">
             <div class="card">
@@ -19,7 +51,7 @@
                 </div>
                 <div class="card-body">
                     <div class="table-responsive">
-                        <table id="dataTableExample" class="table mb-3">
+                        <table id="dataTableExample" class="table">
                             <thead>
                                 <tr class="text-center">
                                     <th>#</th>
@@ -34,11 +66,11 @@ $count = 0;
                             ?>
                             <tbody>
                                 @foreach ($getRecord as $value)
-                                    <tr class="text-start">
+                                    <tr class="text-center">
                                         <td>{{++$count}}</td>
                                         <td>{{$value->name}}</td>
                                         <td>{{$value->email}}</td>
-                                        <td>{{$value->created_at}}</td>
+                                        <td>{{date('d-m-Y H:i A', strtotime($value->created_at))}}</td>
                                         <td>
                                             <a class="btn" href="{{url('admin/admin/edit/' . $value->id)}}">
                                                 <i data-feather="edit" class="link-icon text-warning"></i>
@@ -52,7 +84,9 @@ $count = 0;
                             </tbody>
                         </table>
 
-                        {!!$getRecord->appends(Illuminate\Support\Facades\Request::except('page'))->links()!!}
+                        <div style="padding: 10px; float: right;">
+                            {!!$getRecord->appends(Illuminate\Support\Facades\Request::except('page'))->links()!!}
+                        </div>
 
                     </div>
                 </div>
