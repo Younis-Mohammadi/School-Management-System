7a823ac8164ad6688695e4330de289cd35a10152
Younis-Mohammadi <artacodding2023@gmail.com>
2024-06-04 16:41:24 +0430

    some changes on  Students are showable for teacher with status

--- resources/views/teacher/my_student.blade.php
+++ resources/views/teacher/my_student.blade.php
@@ -13,7 +13,7 @@
         <div class="col-md-12 grid-margin stretch-card">
             <div class="card">
                 <div class="card-header d-flex justify-content-between align-items-center p-4">
-                    <h3 class="mb-3">My Students</h3>
+                    <h3 class="mb-3">My Students List</h3>
                 </div>
                 <div class="card-body">
                     <div class="table-responsive">
